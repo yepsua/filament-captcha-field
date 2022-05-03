d5685fc6a0eb96036c8fba44bc7e55eb98f7c9a8
Omar Yepez <oyepez003@gmail.com>
2022-05-03 15:56:21 -0500

    Adds the mews/captcha dependency

--- src/Forms/Components/Captcha.php
+++ src/Forms/Components/Captcha.php
@@ -6,5 +6,5 @@ use Filament\Forms\Components\Field;
 
 class Captcha extends Field
 {
-    protected string $view = 'filament-captcha-field.forms.components.captcha';
+    protected string $view = 'filament-captcha-field::forms.components.captcha';
 }

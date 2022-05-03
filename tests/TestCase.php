d2b76c30d2b719aba1a89937c61c1e1d08349d61
Omar Yepez <oyepez003@gmail.com>
2022-05-03 15:45:01 -0500

    Adds Filament form component

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -23,10 +23,5 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_filament-captcha-field_table.php.stub';
-        $migration->up();
-        */
     }
 }
